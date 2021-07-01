3a7a97c05d8217199e3e8f49cf43bba963bb165a
John <coldlombax@gmail.com>
2021-07-01 10:20:26 +0100

    About added, app layout templated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const App = () => (
-  <div />
-);
+import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  );
+}
 
 export default App;
