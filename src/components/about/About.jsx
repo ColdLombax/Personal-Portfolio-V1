5193b2655c5d67e4b3b3936f8129c49b4e1f8cc5
John <coldlombax@gmail.com>
2021-07-01 16:58:15 +0100

    Screen width and height to fill with content.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function About() {
   return (
-    <>
+    <div className="h-screen w-full absolute">
       <Header />
-    </>
+      <Main />
+      <Footer />
+    </div>
   );
 }
 
 export default About;
+
+// Attempt to get the screen size working correctly so you can bottom left a curved image.
