4d1e15a81ce0ebfda4d7e27fd089163f7f949ed3
John <coldlombax@gmail.com>
2021-07-01 22:12:36 +0100

    Changeable states for inner content.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 function About() {
+  const [content, setContent] = useState(1);
   return (
     <div className="h-screen w-full absolute">
       <Header />
-      <Main />
-      <Footer />
+      <Main content={content} />
+      <Footer setContent={setContent} />
     </div>
   );
 }
