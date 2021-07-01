d392b1efd58daaa239d53a89cf865149560650c4
John <coldlombax@gmail.com>
2021-07-01 08:32:23 +0100

    Custom theme added with colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple-dark': '#3F3D56',
+        'purple-light': '#575A89',
+        'blue-dark': '#00ACDD',
+        'blue-light': '#19CDFF',
+        'green-dark': '#2F9A2D',
+        'green-light': '#7FFF7C',
+      },
+    },
   },
   variants: {
     extend: {},
