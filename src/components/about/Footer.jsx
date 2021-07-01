be1dd805b785b0a9b9c493ffba27d5b8d3a0a509
John <coldlombax@gmail.com>
2021-07-01 22:13:25 +0100

    Footer changes value based on arrow click for description state change.

--- src/components/about/Footer.jsx
+++ src/components/about/Footer.jsx
@@ -1,8 +1,18 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import arrow from '../../assets/illustrations/arrow.svg';
 
-function Footer() {
+function Footer({ setContent }) {
+  const clickHandler = () => {
+    setContent((prevState) => {
+      if (prevState === 3) {
+        return 1;
+      }
+      return prevState + 1;
+    });
+  };
   return (
     <footer className="
     bg-purple-dark absolute bottom-0 right-0 w-3/12 z-0
@@ -11,10 +21,14 @@ function Footer() {
     "
     >
       <ul className="flex justify-end gap-10 p-2">
-        <img src={arrow} alt="" className="w-10" />
+        <img src={arrow} alt="" className="w-10" onClick={clickHandler} onKeyPress={clickHandler} role="navigation" />
       </ul>
     </footer>
   );
 }
+
+Footer.propTypes = {
+  setContent: PropTypes.func.isRequired,
+};
 
 export default Footer;
